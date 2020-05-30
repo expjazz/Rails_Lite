34ea42956c0e7f4c66be7c9f2c7dcd1500f65932
Expedito Andrade <expeditojazz@gmail.com>
2020-05-30 09:26:28 -0300

    Added method 1 for session

--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,25 @@
+# frozen_string_literal: true
+
 require 'json'
 
 class Session
+  attr_accessor :cookie, :cookie_val
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @cookie = req.cookies['_rails_lite_app']
+    if @cookie
+      @cookie_val = JSON.parse(@cookie)
+    else
+      res.set_cookie(_rails_lite_app, path: '/', value: {}.to_json)
+    end
   end
 
-  def [](key)
-  end
+  def [](key); end
 
-  def []=(key, val)
-  end
+  def []=(key, val); end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
-  def store_session(res)
-  end
+  def store_session(res); end
 end
